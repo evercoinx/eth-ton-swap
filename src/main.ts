6a8559a4fde9fe2e695732ffd8436105b33dd8e0
Serge Grigorenko <serge.grigorenko@gmail.com>
2022-02-07 20:55:22 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: {
+        prettyPrint:
+          process.env.NODE_ENV === 'development'
+            ? {
+                translateTime: 'HH:MM:ss Z',
+                ignore: 'pid,hostname',
+              }
+            : false,
+      },
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
