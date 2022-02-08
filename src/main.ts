3d9f1ce352693da5f9b6f38d81bc79133bf624b3
Serge Grigorenko <serge.grigorenko@gmail.com>
2022-02-08 17:57:15 +0200

    Update prettier configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import "dotenv/config"
 import { NestFactory } from "@nestjs/core"
-import {
-	FastifyAdapter,
-	NestFastifyApplication,
-} from "@nestjs/platform-fastify"
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
