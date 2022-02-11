caccb804c245c5b5d00fd63d34efa03711118d68
Serge Grigorenko <serge.grigorenko@gmail.com>
2022-02-11 17:29:39 +0200

    Update logging in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,11 @@
-import "dotenv/config"
-import { ValidationPipe } from "@nestjs/common"
+import { Logger, ValidationPipe } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
 import { NestFactory } from "@nestjs/core"
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify"
-import { AppModule, Environment } from "./app.module"
+import { AppModule } from "./app.module"
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestFastifyApplication>(
-		AppModule,
-		new FastifyAdapter({
-			logger: {
-				prettyPrint:
-					process.env.NODE_ENV === Environment.Development
-						? {
-								translateTime: "HH:MM:ss",
-								ignore: "pid,hostname",
-						  }
-						: false,
-			},
-		}),
-	)
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
 	app.useGlobalPipes(
 		new ValidationPipe({
@@ -30,5 +16,6 @@ async function bootstrap() {
 
 	const configService = app.get(ConfigService)
 	await app.listen(configService.get<number>("application.port"))
+	Logger.log(`Application is running on ${await app.getUrl()}`, "Bootstrap")
 }
 bootstrap()
