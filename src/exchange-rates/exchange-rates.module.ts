46e304d61532cad9baa28aea01304a88cfa81b3b
Serge Grigorenko <serge@evercloud.io>
2022-02-12 21:17:21 +0200

    Update configuration of exchange rates module

--- src/exchange-rates/exchange-rates.module.ts
+++ src/exchange-rates/exchange-rates.module.ts
@@ -1,10 +1,22 @@
 import { HttpModule } from "@nestjs/axios"
-import { ConfigModule } from "@nestjs/config"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 import { Module } from "@nestjs/common"
 import { ExchangeRatesService } from "./exchange-rates.service"
 
 @Module({
-	imports: [ConfigModule, HttpModule],
+	imports: [
+		HttpModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				headers: {
+					"X-CMC_PRO_API_KEY": configService.get("coinmarketcap.apiKey"),
+				},
+				timeout: 5000,
+				maxRedirects: 1,
+			}),
+		}),
+	],
 	providers: [ExchangeRatesService],
 	exports: [ExchangeRatesService],
 })
--- src/exchange-rates/exchange-rates.service.ts
+++ src/exchange-rates/exchange-rates.service.ts
@@ -1,6 +1,5 @@
 import { HttpService } from "@nestjs/axios"
 import { Injectable, Logger } from "@nestjs/common"
-import { ConfigService } from "@nestjs/config"
 import { AxiosResponse } from "axios"
 import { firstValueFrom } from "rxjs"
 import { map } from "rxjs/operators"
@@ -11,41 +10,35 @@ enum Token {
 	USDC = "USDC",
 }
 
+const TOKEN_TO_CMCID = {
+	[Token.Toncoin]: 11419,
+	[Token.USDC]: 3408,
+}
+
 @Injectable()
 export class ExchangeRatesService {
 	private readonly logger = new Logger(ExchangeRatesService.name)
 	private readonly coinmarketcapEndpoint = "https://api.coinmarketcap.com"
 
-	constructor(private configSerivce: ConfigService, private httpService: HttpService) {}
+	constructor(private httpService: HttpService) {}
 
 	async getQuotePrice(base: Token, quote: Token): Promise<number | undefined> {
-		const tokenToCmcId = {
-			[Token.Toncoin]: 11419,
-			[Token.USDC]: 3408,
-		}
-
 		const url = new URL(
-			`${this.coinmarketcapEndpoint}/data-api/v3/tools/price-conversion?amount=1&id=${tokenToCmcId[base]}&convert_id=${tokenToCmcId[quote]}`,
+			`${this.coinmarketcapEndpoint}/data-api/v3/tools/price-conversion?amount=1&id=${TOKEN_TO_CMCID[base]}&convert_id=${TOKEN_TO_CMCID[quote]}`,
 		)
 
-		const price$ = this.httpService
-			.get(url.href, {
-				headers: {
-					"X-CMC_PRO_API_KEY": this.configSerivce.get("coinmarketcap.apiKey"),
-				},
-			})
-			.pipe(
-				map((response: AxiosResponse<PriceConversion>) => {
-					const { data } = response
-					if (data.status.error_code !== "0" || !data.data.quote.length) {
-						this.logger.error(
-							`Code: ${data.status.error_code}. Message: ${data.status.error_message}`,
-						)
-						return
-					}
-					return data.data.quote[0].price
-				}),
-			)
+		const price$ = this.httpService.get(url.href).pipe(
+			map((response: AxiosResponse<PriceConversion>) => {
+				const { data } = response
+				if (data.status.error_code !== "0" || !data.data.quote.length) {
+					this.logger.error(
+						`Code: ${data.status.error_code}. Message: ${data.status.error_message}`,
+					)
+					return
+				}
+				return data.data.quote[0].price
+			}),
+		)
 
 		return await firstValueFrom(price$)
 	}
