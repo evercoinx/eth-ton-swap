7e766e02e994d9c79eddd505e48e0f2444d83cd8
Serge Grigorenko <serge@evercloud.io>
2022-02-25 20:22:31 +0300

    Rename interface files

--- src/ton/ton.service.ts
+++ src/ton/ton.service.ts
@@ -1,14 +1,13 @@
 import { Inject, Injectable } from "@nestjs/common"
-import { options } from "joi"
 import { contract, HttpProvider, providers, Wallets } from "tonweb"
 import nacl from "tweetnacl"
 import { TON_MODULE_OPTIONS } from "./constants"
 import { TonModuleOptions } from "./interfaces/ton-module-options.interface"
-import { TonWalletData } from "./interfaces/ton-wallet-data"
+import { TonWalletData } from "./interfaces/ton-wallet-data.interface"
 
 @Injectable()
 export class TonService {
-	private readonly wallet: typeof contract.WalletContract
+	private readonly Wallet: typeof contract.WalletContract
 	private readonly workchain: number
 	private readonly httpProvider: providers.HttpProvider
 
@@ -17,18 +16,18 @@ export class TonService {
 		this.httpProvider = new HttpProvider(host)
 
 		const wallets = new Wallets(this.httpProvider)
-		this.wallet = wallets.all[options.walletVersion]
+		this.Wallet = wallets.all[options.walletVersion]
 	}
 
 	createRandomWallet(): TonWalletData {
 		const keyPair = nacl.sign.keyPair()
-		const tonWallet = new this.wallet(this.httpProvider, {
+		const wallet = new this.Wallet(this.httpProvider, {
 			publicKey: keyPair.publicKey,
 			wc: this.workchain,
 		})
 
 		return {
-			wallet: tonWallet,
+			wallet,
 			secretKey: Buffer.from(keyPair.secretKey).toString("hex"),
 		}
 	}
