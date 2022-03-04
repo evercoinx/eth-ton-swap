eb6aed4fa4b57835ca1fc5cb2e3e01f249d2ce90
Serge Grigorenko <serge@evercloud.io>
2022-03-04 21:20:08 +0300

    Refactor TON module

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -7,15 +7,7 @@ import { WalletsController } from "./wallets.controller"
 import { WalletsService } from "./wallets.service"
 
 @Module({
-	imports: [
-		TypeOrmModule.forFeature([Wallet]),
-		TokensModule,
-		TonModule.register({
-			isTestnet: true,
-			workchain: 0,
-			walletVersion: "v3R2",
-		}),
-	],
+	imports: [TypeOrmModule.forFeature([Wallet]), TokensModule, TonModule.register()],
 	controllers: [WalletsController],
 	providers: [WalletsService],
 	exports: [WalletsService],
